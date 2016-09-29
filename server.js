ffb39811b9979907ecf3d0c7174ed7f0a40411d2
Jharbison <jeff.p.harbison@gmail.com>
2016-09-29 18:38:17 -0400

    change port

--- server.js
+++ server.js
@@ -73,6 +73,6 @@ app.put('/items/:id', jsonParser, function(request, response){
 });
 
 
-app.listen("3030", function(){
+app.listen((process.env.PORT || 3030), function(){
 	console.log("App is running at http://localhost:3030")
 });
